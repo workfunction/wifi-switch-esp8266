3ab3ddf6da1114b3a53c31199f4e90662165da9d
Jason Hunag <jason840507@gmail.com>
2021-07-25 16:04:58 +0800

    Change repo structure

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
 #include "server.h"
 #include "eeprom.h"
 #include "wifi.h"
+#include "ota.h"
 
 void setup(void)
 {
@@ -9,10 +10,12 @@ void setup(void)
     EEPROM.begin(EEPROM_SIZE);
 
     int wifi_stat = wifi_init();
+    ota_setup();
     server_init();
 }
 
 void loop(void)
 {
+    ota_loop();
     server_loop();
 }
